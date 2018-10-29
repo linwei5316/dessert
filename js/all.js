7f80ac9b8797fd49f6b02aaffa8bb7a8d077a040
leo <linwei5316@gmail.com>
2018-10-30 00:12:37 +0800

    initial commit

---
+++ js/all.js
@@ -0,0 +1,5 @@
+document.addEventListener('DOMContentLoaded',function(){
+  document.querySelector('.menu-button').addEventListener('click',function(){
+    document.querySelector('.navigation').classList.toggle('menu-open')
+  })
+})
